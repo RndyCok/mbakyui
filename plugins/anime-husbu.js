78408177232aa44dfe6a4ba56b0a14b4f0702ae6
RndyCok <randygans071@gmail.com>
2022-07-04 17:46:01 +0700

    iyhbanng

---
+++ plugins/anime-husbu.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let res = await fetch('https://melcanz.com/husbu?&apikey=melcantik')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    //conn.sendButton(m.chat, 'Suami kok kartun', botdate + '\n\n' + wm, json.url, [['Husbu', `${usedPrefix}husbu`]], m)
+    conn.sendButton(m.chat, 'Suami kok kartun', botdate + '\n\n' + wm, json.url, [['Husbu', `${usedPrefix}husbu`]], m, {
+contextInfo: { externalAdReply :{
+                        description: stickauth,
+                        title: author,
+                        body: 'Jangan lupa Follow',
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: sig }}})
+}
+handler.help = ['husbu']
+handler.tags = ['anime']
+handler.command = /^(husbu)$/i
+export default handler
